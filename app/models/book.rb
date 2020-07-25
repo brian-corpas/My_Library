7621be5c537533380bd6e4ba27899388a0c7f276
Brian Corpas <briancorpas@gmail.com>
2020-07-25 11:50:36 +0100

    relation tables between books and users through loans added

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
-  belongs_to :user
+  has_many :loans
+  has_many :users, through: :loans
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_090049) do
+ActiveRecord::Schema.define(version: 2020_07_25_104724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "name"
+    t.string "author"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "loans", force: :cascade do |t|
+    t.string "name"
+    t.string "date"
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_loans_on_book_id"
+    t.index ["user_id"], name: "index_loans_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +47,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_090049) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
+  add_foreign_key "loans", "books"
+  add_foreign_key "loans", "users"
 end
