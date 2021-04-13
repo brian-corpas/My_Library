3993f9f8726c2cdd01ba2b72a74c9b622010635d
brian corpas <briancorpas@gmail.com>
2021-04-13 01:52:01 +0100

    wishlist done

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
 
   has_many :books
   has_many :comics
+  has_many :wishlists
   has_many :loans, through: :books
   has_one_attached :photo
 end
---
+++ app/policies/wishlist_policy.rb
@@ -0,0 +1,22 @@
+class WishlistPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  def index?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def destroy?
+    true
+  end
+end
